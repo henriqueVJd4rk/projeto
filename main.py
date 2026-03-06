c800a16c18ff160f879635b80c8bfedf2de7bd77
henriqueVJd4rk <henrique.jacomini@aluno.senai.br>
2026-03-06 10:18:01 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<h1>Hello World!!!</h1>"
+
+@app.route("/curso")
+def curso():
+    return "<h1>Henrique Jacomini</h1>"\
+    "<h2> TI </h2>"
+
 if __name__== "__main__":
     app.run()
